c2f35ca6bab0330ef936571d3ec414643b774705
danielpereira16 <17022@my.sanctamaria.school.nz>
2022-02-10 12:15:20 +1300

    Updated Pizza Bot
    Removed code to make separate sections for each component

---
+++ Components/welcome.py
@@ -0,0 +1,13 @@
+import random
+from random import randint
+
+#List of random names
+names = ["Daniel", "Caleb", "Jayden", "Carlos", "Louis", "Keira", "Danett", "Hannah", "Francesca", "Ella" ]
+
+num = randint(0,9)
+
+name = (names[num])
+
+print("*** Welcome to Dream Pizza ***")
+print("*** my name is ",name, "***")
+print("*** I will be here to help you order your delicious Dream Pizza ***")
--- pizzabot.py
+++ pizzabot.py
@@ -1,1 +1,2 @@
 # Pizza Bot Program
+
