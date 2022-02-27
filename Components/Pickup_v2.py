68009bd3bbd5cb8cf8694dba4993d04a96988bb7
danielpereira16 <17022@my.sanctamaria.school.nz>
2022-02-28 12:45:51 +1300

    Added "Not Blank"
    Added validation so that entry cannot be blank

---
+++ Components/Pickup_v2.py
@@ -0,0 +1,28 @@
+#Bug - accepts blank input
+
+print("Please enter the pickup information")
+
+
+
+
+#customer name
+valid = False
+while not valid:
+    name = input("Please enter you name ")
+    if name != "":
+        print (name)
+        break
+    else:
+        print("Sorry this cannot be blank")
+
+
+#customer phone number
+valid = False
+while not valid:
+        phone = input("Please enter your phone number ")
+        if phone != "":
+            print (phone)
+            break
+        else:
+            print("Sorry this cannot be blank")    
+
