2c0f3bc1c62829bfa6cde9e4922b3638b70545d4
danielpereira16 <17022@my.sanctamaria.school.nz>
2022-03-02 22:26:06 +1300

    Integrated pickup menu into main app
    Made pickup or delivery menu into a function and integrated it into main routine

--- pizzabot.py
+++ pizzabot.py
@@ -2,24 +2,9 @@
 import random
 from random import randint
 
-#Customer details dictionary
-customer_details = {}
-
 #List of random names
 names = ["Daniel", "Caleb", "Jayden", "Carlos", "Louis", "Keira", "Danett", "Hannah", "Francesca", "Ella" ]
 
-#Validates inputs to check if they are blank
-def not_blank(question):
-    valid = False
-    while not valid:
-        response = input(question)
-        if response != "":
-            return response
-        else:
-            print("This cannot be blank")
-
-
-# Welcome messsage with random name
 def welcome():
     '''
     Purpose: To generate a random name form the list and print out
@@ -34,23 +19,35 @@ def welcome():
     print("*** I will be here to help you order your delicious Dream Pizza ***")
 
 
-# Menu for pick up or delivery
-def order_type():
+#Menu for pickup or delivery
+
+def pickup():
+    print ("Is your order for pickup or delivery")
+    print ("For pickup please enter 1")
+    print ("For delivery please enter 2")
+    while True:
+        try:
+            delivery = int(input("Please enter a number "))
+            if delivery >= 1 and delivery <= 2:
+                if delivery == 1:
+                    print ("Pickup")
+                    break
+
+                elif delivery == 2:
+                    print ("Delivery")
+                    break
+            else: 
+                print("Number must be 1 or 2")
+        except ValueError:
+            print("That is not valid number")
+            print("Please enter 1 or 2 ")
 
 
 
 
 
 # Pick up information - name and phone number
-def pickup():
-    question = ("Please enter your name ")
-    customer_details['name'] = not_blank(question )
-    #print(customer_details['name'])
 
-    question = ("Please enter your phone number ")
-    customer_details['phone'] = not_blank(question)
-    #print(customer_details['phone'])
-    print(customer_details)
 
 
 
@@ -98,6 +95,9 @@ def pickup():
 
 
 
+
+
+
 # Main Function
 def main():
     '''
@@ -107,7 +107,6 @@ def main():
     Returns: None
     '''
     welcome()
-    order_type()
     pickup()
 
 
